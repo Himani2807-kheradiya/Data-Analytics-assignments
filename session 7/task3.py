60257a035f0fa7e0df4578ab3ca6e3b86c17365c
Himani2807-kheradiya <himikheradiya2806@gmail.com>
2026-08-12 18:51:34 +0530

    ..

---
+++ session 7/task3.py
@@ -0,0 +1,10 @@
+# Task 3: Zomato Free Delivery
+
+order_total = float(input("Enter your Zomato order total: "))
+
+if order_total > 299:
+    print("Apply Free Delivery")
+elif order_total >= 200:
+    print("Add more items for free delivery")
+else:
+    print("Delivery charges apply")
---
+++ session 7/task4.py
@@ -0,0 +1,10 @@
+cart_value = float(input("Enter Flipkart cart value: "))
+payment_method = input("Enter payment method (UPI/Card/Cash): ")
+
+if cart_value > 1000:
+    if payment_method == "UPI":
+        print("Eligible for 10% cashback")
+    else:
+        print("Eligible for 5% cashback")
+else:
+    print("No cashback")
